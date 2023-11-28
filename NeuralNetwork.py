8eb7bf91ebeeb00263fe6261a4aea7ab1ad05e00
Andy-xiong6 <xxxionghw@gmail.com>
2023-11-28 15:42:58 +0800

    add test code, batch size to be fixed

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -13,14 +13,15 @@ class MLP:
             elif activation == 'relu':
                 self.activation = MLP.functions.relu
                 
-            self.weights = np.random.rand(n_input, n_output)
-            self.bias = np.random.rand(n_output)
+            self.weights = np.random.rand(n_input, n_output) # n_input x n_output
+            self.bias = np.random.rand(n_output) # n_output x 1
 
         def forward(self, x):
-            self.x = x
-            self.z = np.dot(x, self.weights) + self.bias # z = w * x + b
-            self.a = self.activation(self.z) # a = f(z)
+            self.x = x # n_input x 1
+            self.z = np.dot(x, self.weights) + self.bias # n_output x 1
+            self.a = self.activation(self.z) # n_output x 1
             return self.a
+        
         
         def backward(self, da):
             dz = da * self.activation(self.z, derivative=True) # dz = da * f'(z)
@@ -28,21 +29,23 @@ class MLP:
             db = np.sum(dz, axis=0) # db = sum(dz)
             dx = np.dot(dz, self.weights.T) # dx = dz * w.T
             return dx, dw, db
+
         
-        def update(self, dw, db, alpha):
-            self.weights -= alpha * dw
-            self.bias -= alpha * db
-        
-        def momentum_update(self, dw, db, alpha, gama, vdw, vdb):
-            vdw = gama * vdw + alpha * dw
-            vdb = gama * vdb + alpha * db
-            self.weights -= alpha * vdw
-            self.bias -= alpha * vdb
+        def update(self, dw, db, alpha, momentum=False, gama=None, vdw=None, vdb=None):
+            if momentum:
+                vdw = gama * vdw + alpha * dw
+                vdb = gama * vdb + alpha * db
+                self.weights -= alpha * vdw
+                self.bias -= alpha * vdb
+            else:
+                self.weights -= alpha * dw
+                self.bias -= alpha * db
     
     class Layer:
         def __init__(self, n_input, n_output, activation, momentum=False, OutputMode=False):
             self.n_input = n_input
             self.n_output = n_output
+            self.momentum = momentum
             
             if activation == 'sigmoid':
                 self.activation = MLP.functions.sigmoid
@@ -57,18 +60,16 @@ class MLP:
                 self.neurons.append(neuron)
                 
             if OutputMode:
-                self.cost = MLP.functions.cross_entropy
+                self.loss = MLP.functions.cross_entropy
             else:
-                self.cost = MLP.functions.MSE
+                self.loss = MLP.functions.MSE
             
         def forward(self, x):
-            self.x = x
+            self.x = x # n_input x 1
             a = []
-            
             for neuron in self.neurons:
                 output = neuron.forward(x)
                 a.append(output)
-
             self.a = np.array(a).T
             return self.a 
         
@@ -83,19 +84,20 @@ class MLP:
                 dw.append(gradient[1])
                 db.append(gradient[2])
                 
-            dx = np.sum(dx, axis=0)
-            dw = np.sum(dw, axis=0)
-            db = np.sum(db, axis=0)
+            dx = np.sum(dx, axis=0) # n_input x 1
+            dw = np.sum(dw, axis=0) # n_input x n_output
+            db = np.sum(db, axis=0) # n_output x 1
         
             return dx, dw, db
         
-        def update(self, dw, db, alpha):
-            for neuron in self.neurons:
-                neuron.update(dw, db, alpha)
-                
-        def momentum_update(self, dw, db, alpha, gama, vdw, vdb):
-            for neuron in self.neurons:
-                neuron.momentum_update(dw, db, alpha, gama, vdw, vdb)
+        def update(self, dw, db, alpha, momentum=False, gama=None, vdw=None, vdb=None):
+            if momentum:
+                for neuron in self.neurons:
+                    neuron.update(dw, db, alpha, momentum, gama, vdw, vdb)
+            else:
+                for neuron in self.neurons:
+                    neuron.update(dw, db, alpha)
+                    
     class functions:
         def sigmoid(self, z , derivative=False):
             if derivative:
@@ -131,13 +133,14 @@ class MLP:
         self.n_input = n_input
         self.n_output = n_output
         self.hidden_number = hidden_number
+        self.layers = []
         for i in range(len(hidden_number)):
             activation = activations[i]
-            if i == 0:
+            if i == 0: # input layer
                 layer = MLP.Layer(n_input, hidden_number[i], activation)
-            elif i != len(hidden_number) - 1:
+            elif i != len(hidden_number) - 1: # hidden layers
                 layer = MLP.Layer(hidden_number[i-1], hidden_number[i], activation)
-            else:
+            else: # output layer
                 layer = MLP.Layer(hidden_number[i], n_output, activation , OutputMode=True)
             self.layers.append(layer)
         
@@ -146,19 +149,21 @@ class MLP:
             x = layer.forward(x)
         return x
     
-    def backward(self, y, y_hat):
+    def backward(self, y, y_hat, learning_rate):
         dy = y - y_hat
         for layer in reversed(self.layers):
             dy, dw, db = layer.backward(dy)
-            layer.update(dw, db, 0.01)
+            layer.update(dw, db, learning_rate)
     
-    def train(self, x, y, epochs):
+    def train(self, x, y, epochs, learning_rate):
         for i in range(epochs):
             y_hat = self.forward(x)
-            self.backward(y, y_hat)
+            self.backward(y, y_hat, learning_rate)
     
     def predict(self, x):
         return self.forward(x)
+    
+    def 
             
 
     
--- __init__.py
+++ __init__.py
@@ -1,7 +1,9 @@
 from LogisticRegression import MCLogisticRegression
 from NeuralNetwork import MLP
 import numpy as np
-
+'''
+LogisticRegression on binary classification
+'''
 #import data
 train_data = np.loadtxt('train.data', delimiter=',')
 train_input = train_data[:,0:2]
@@ -30,3 +32,37 @@ print(y_hat)
 #accuracy
 accuracy = np.sum(np.argmax(y_hat, axis=1) == np.argmax(test_label, axis=1)) / len(test_data)
 print("accuracy: ", accuracy)
+
+'''
+Multi-layer Perceptron with single hidden layer but different numbers of neurons
+'''
+#import data
+train_data = np.loadtxt('train.data', delimiter=',')
+train_input = train_data[:,0:2].T
+train_label = train_data[:,2]
+
+test_data = np.loadtxt('test.data', delimiter=',')
+test_input = test_data[:,0:2].T
+test_label = test_data[:,2]
+
+
+def one_hidden_layer_train(activation_function, epoch, learning_rate):
+    hidden_numbers = []
+    activations = []
+    for i in range(1,21):
+        hidden_numbers.clear()
+        hidden_numbers.append(i)
+        activations.append(activation_function)
+        mlp = MLP(len(train_input), len(train_label), hidden_numbers, activations)
+        mlp.train(train_input, train_label, epoch, learning_rate)
+        mlp.predict(test_input)
+        accuracy = np.sum(np.argmax(mlp.y_hat, axis=1) == np.argmax(test_label, axis=1)) / len(test_data)
+        print("accuracy: ", accuracy)
+    
+#using sigmoid as the activation function
+one_hidden_layer_train('sigmoid', 1000, 0.1)
+#using tanh as the activation function
+one_hidden_layer_train('tanh', 1000, 0.1)
+#using relu as the activation functino
+one_hidden_layer_train('relu', 1000, 0.1)
+
